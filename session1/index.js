7057a91e01adcfc6d0bfcb900a025d5a613eb166
Fandedong <haokute212k@gmail.com>
2019-05-14 07:06:03 -0800

    nop bai

---
+++ session1/index.js
@@ -0,0 +1,22 @@
+// let name = 1
+// name = 5
+// console.log("hello",name);
+
+// let x = Number(prompt(" Nhập số thứ 1" ));
+// let y = Number(prompt(" Nhập số thứ 2" ));
+// console.log(x + y)
+clear()
+
+for (let i =0; i < 3;i=i+2) {
+fd(100)
+rt(90)
+fd(100)
+rt(90)
+fd(100)
+rt(90)
+fd(100)
+rt(90)
+
+rt(30)
+
+}
