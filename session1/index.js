13282f63ac1c169ed22036e9589b80713947e00c
Fandedong <50629471+Fandedong@users.noreply.github.com>
2019-05-19 04:38:34 -0500

    Add files via upload

--- session1/index.js
+++ session1/index.js
@@ -1,13 +1,24 @@
-// let name = 1
-// name = 5
-// console.log("hello",name);
-
-// let x = Number(prompt(" Nhập số thứ 1" ));
-// let y = Number(prompt(" Nhập số thứ 2" ));
-// console.log(x + y)
+// session1
+// 1
+// 1.1
+// typeof(X)
+// 1.2: trong trường hợp tên biến  bắt đầu bằng chữ số, tên biến chứa kí tự dặc biệt.
+// vd 
+// var 1asd;
+// var a-d;
+// var 1,a;
+// 1.3 :the area of a circle
+let radius= prompt( "Radius?")
+const pi = 3.14
+let Area = radius*radius*pi
+console.log(Area)
+// 1.4 converts Celsius (0C) into Fahrenheit (0F)
+let Celsius = Number(prompt("Enter the tepature in Celsius?"))
+let Fahrenheit = Celsius*1.8 + 32
+console.log(Fahrenheit)
+// 1.5
+// square
 clear()
-
-for (let i =0; i < 3;i=i+2) {
 fd(100)
 rt(90)
 fd(100)
@@ -16,7 +27,13 @@ fd(100)
 rt(90)
 fd(100)
 rt(90)
-
+// triangle
+clear()
 rt(30)
-
-}
+fd(100)
+rt(120)
+fd(100)
+rt(120)
+fd(100)
+rt(120)
+// circle
