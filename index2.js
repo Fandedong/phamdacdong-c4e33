908afe3c9d999162f108c7ab7bfa3d1523d3aaa9
Fandedong <50629471+Fandedong@users.noreply.github.com>
2019-05-27 14:27:58 -0500

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,38 @@
+// // 1.
+// Boolean là kiểu dữu liệu chỉ có 2 giá trị true hoặc false. 
+// Ex (2 > 1)
+// ("")
+// (x = -0) 
+
+// // 2.  A flowchart Flow chart là một dạng biểu đồ có tính hữu dụng lớn cho các lập trình viên, đặc biệt trong các hệ thống sử dụng phương pháp lập trình hướng chức năng (OOF)
+
+// name -- “Huy be"-------- print(“Hand some")
+// 		|				  true
+// 		|
+// 		|----------- name -- “Huy big"--------------- even_more_handsome - True
+// 				  false						 |		   true
+// 											 |
+// 					  						 |false
+// 					  						 |
+// 					  						 |		
+// 					  webbrowser.open(“https://www.youtube.com/watch?v=04854XqcfCY”)
+
+// // 3
+
+// Nested conditionals is when if() is put inside another if()
+
+let usn = prompt("Enter your username: ")
+if (usn === "C4E33"){ 
+    let pw = prompt("Enter your passwords: ")
+    if (pw === "codethechange"){
+        console.log("Welcome!")
+    }
+    else{
+        console.log("Wrong Passwords")
+    }
+    }
+else{ 
+    console.log("Not found!")
+}
+
+
